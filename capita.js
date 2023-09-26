21cbe1bc4072ec603869c03060c0ead55ca93ec1
GiovanniSmokes <138458840+GiovanniSmokes@users.noreply.github.com>
2023-09-26 01:19:33 +0100

    Add files via upload

---
+++ capita.js
@@ -0,0 +1,10 @@
+var CHATBOT_EMAIL = "apssouza22@gmail.com" 
+var APP_KEY ='87644985'
+
+function loadChatScript(url) {
+    let script = document.createElement('script');
+    script.type = 'module';
+    script.src = url;
+    document.head.appendChild(script);
+}
+loadChatScript("https://apps.newaisolutions.com/assets/js/chatbot-plugin.js");
